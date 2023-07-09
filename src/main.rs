3d8c59b3e4b90302da7514c2adaac19dcfd1e98c
zimward <96021122+zimward@users.noreply.github.com>
2023-07-09 14:03:24 +0200

    proper parameterization of heat src

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,20 @@
-use std::{fs::read_to_string, io::Error, path::PathBuf};
+use std::{
+    fs::{read_to_string, write},
+    io::Error,
+    path::PathBuf,
+};
 
 use deps::check_dependencies;
-use dialoguer::Confirm;
+use dialoguer::{console::TermFamily, Confirm, Input, Select};
 use glob::glob;
 
 mod deps;
 
-fn search_fans() -> Vec<String> {
-    Vec::default()
-}
-
-fn ask_heat_src(path: &PathBuf) -> Result<bool, Error> {
-    let mut value = read_to_string(path)?;
-    value.pop();
-    let mut value: f32 = value.parse().unwrap_or(0.0);
-    value /= 1000.0;
-    let label = path.to_str().unwrap_or("");
-    let label = label.replace("input", "label");
-    let name = read_to_string(label)?;
-    println!(
-        "Found source {}",
-        path.canonicalize()?.to_str().unwrap_or("")
-    );
-    println!("Current value:{value}°C. Label:{name}");
-    Ok(Confirm::new()
-        .with_prompt("Do you want to add this heat source to config?")
-        .default(true)
-        .interact()
-        .unwrap_or(false))
-}
-
-fn search_paths<F>(possible_paths: &[&str], question_fn: F) -> Vec<String>
+fn search_paths<F, R>(possible_paths: &[&str], question_fn: F) -> Vec<R>
 where
-    F: Fn(&PathBuf) -> Result<bool, Error>,
+    F: Fn(&PathBuf) -> Result<Option<R>, Error>,
 {
-    let mut srcs: Vec<String> = Vec::default();
+    let mut srcs: Vec<R> = Vec::default();
     for path in possible_paths.iter() {
         //find matches
         let paths = glob(path);
@@ -52,24 +32,137 @@ where
             }
         });
         for path in paths {
-            if let Ok(add) = question_fn(&path) {
-                if add {
+            if let Ok(val) = question_fn(&path) {
+                if let Some(toadd) = val {
                     //resolve hwmon path
                     //failing should be impossible at this point
-                    srcs.push(path.canonicalize().unwrap().to_str().unwrap().to_string());
+                    srcs.push(toadd);
                 }
             }
         }
     }
     srcs
 }
+#[allow(dead_code)]
+struct Pid {
+    set_point: f32,
+    p: f32,
+    i: f32,
+    d: f32,
+}
 
-fn search_heat_srcs() -> Vec<String> {
+#[allow(dead_code)]
+struct HeatSrc {
+    name: String,
+    wildcard_path: String,
+    pid_params: Pid,
+}
+impl HeatSrc {
+    fn new(name: String, wildcard_path: String, set_point: f32) -> Self {
+        Self {
+            name,
+            wildcard_path,
+            pid_params: Pid {
+                set_point,
+                p: 0.0,
+                i: 0.0,
+                d: 0.0,
+            },
+        }
+    }
+}
+
+fn ask_heat_src(path: &PathBuf) -> Result<Option<HeatSrc>, Error> {
+    let mut value = read_to_string(path)?;
+    value.pop();
+    let mut value: f32 = value.parse().unwrap_or(0.0);
+    value /= 1000.0;
+    let label = path.to_str().unwrap_or("");
+    let label = label.replace("input", "label");
+    let name = read_to_string(label)?;
+    let full_path = path.canonicalize()?.to_str().unwrap_or("").to_string();
+    println!("Found source {full_path}");
+    println!("Current value:{value}°C. Label:{name}");
+    if Confirm::new()
+        .with_prompt("Do you want to add this heat source to config?")
+        .default(true)
+        .interact()
+        .unwrap_or(false)
+    {
+        //configure heatsrc
+        let name: String = Input::new()
+            .with_prompt("Enter heat pressure source name")
+            .interact_text()?;
+        let set_point: String = Input::new()
+            .with_prompt("Enter setpoint (°C)")
+            .validate_with(|input: &String| {
+                for c in input.chars() {
+                    if c.is_alphabetic() {
+                        return Err("You may only enter a number.");
+                    }
+                }
+                Ok(())
+            })
+            .interact_text()?;
+        Ok(Some(HeatSrc::new(
+            name,
+            full_path,
+            set_point.parse().unwrap_or(60.0),
+        )))
+    } else {
+        Ok(None)
+    }
+}
+
+fn search_heat_srcs() -> Vec<HeatSrc> {
     let possible_paths = ["/sys/class/hwmon/hwmon*/temp*_input"];
     search_paths(&possible_paths, ask_heat_src)
+}
+
+#[allow(dead_code)]
+struct Fan<'a> {
+    name: String,
+    wildcard_path: String,
+    min_pwm: u8,
+    max_pwm: u8,
+    cutoff: bool,
+    heat_pressure_srcs: &'a [&'a str],
+}
+
+impl Default for Fan<'static> {
+    fn default() -> Self {
+        Self {
+            name: "none".to_owned(),
+            wildcard_path: "/dev/null".to_owned(),
+            min_pwm: 0,
+            max_pwm: 255,
+            cutoff: false,
+            heat_pressure_srcs: &[],
+        }
+    }
+}
+
+fn ask_fan(path: &PathBuf) -> Result<Option<Fan<'static>>, Error> {
+    write(path, "255")?;
+    if Confirm::new()
+        .with_prompt("Do you want to add this fan to config?")
+        .default(true)
+        .interact()
+        .unwrap_or(false)
+    {
+        Ok(Some(Fan::default()))
+    } else {
+        Ok(None)
+    }
+}
+
+fn search_fans() -> Vec<Fan<'static>> {
+    let possible_paths = ["/sys/class/hwmon/hwmon3/pwm*[0-9]"];
+    search_paths(&possible_paths, ask_fan)
 }
 
 fn main() {
     check_dependencies();
     search_heat_srcs();
+    search_fans();
 }
