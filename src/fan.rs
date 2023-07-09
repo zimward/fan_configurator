340e236c07c12d27840df1921714cbd4fa2ca49a
zimward <96021122+zimward@users.noreply.github.com>
2023-07-09 20:25:46 +0200

    added fan undulation

--- src/fan.rs
+++ src/fan.rs
@@ -2,23 +2,28 @@ use dialoguer::{Confirm, Input};
 use std::{
     fs::{read_to_string, write},
     io::Error,
-    path::PathBuf,
+    path::{Path, PathBuf},
+    rc::Rc,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
     thread,
     time::Duration,
 };
 
 use crate::heatsrc::search_paths;
 
-pub struct Fan<'a> {
+pub struct Fan {
     name: String,
     wildcard_path: String,
     min_pwm: u8,
     max_pwm: u8,
     cutoff: bool,
-    heat_pressure_srcs: &'a [&'a str],
+    heat_pressure_srcs: Vec<String>,
 }
 
-impl Default for Fan<'static> {
+impl Default for Fan {
     fn default() -> Self {
         Self {
             name: "none".to_owned(),
@@ -26,7 +31,7 @@ impl Default for Fan<'static> {
             min_pwm: 0,
             max_pwm: 255,
             cutoff: false,
-            heat_pressure_srcs: &[],
+            heat_pressure_srcs: Vec::default(),
         }
     }
 }
@@ -39,10 +44,12 @@ fn enable_fan_pwm(path: &PathBuf, enable: bool) -> Result<(), Error> {
         let file = file.to_str().unwrap();
         pwm_enable.pop();
         pwm_enable.push(format!("{file}_enable"));
+        println!("enableing");
         match enable {
             true => write(pwm_enable.as_path(), "1")?,
             false => write(pwm_enable.as_path(), "0")?,
         }
+        println!("finished");
     }
     Ok(())
 }
@@ -65,7 +72,7 @@ fn read_rpm(path: &PathBuf) -> Result<u32, Error> {
     }
 }
 
-fn ask_fan(path: &PathBuf) -> Result<Option<Fan<'static>>, Error> {
+fn ask_fan(path: &PathBuf) -> Result<Option<Fan>, Error> {
     let canonical_path = path.canonicalize()?;
     //enable manual pwm control
     //enable_fan_pwm(path, true)?;
@@ -76,7 +83,7 @@ fn ask_fan(path: &PathBuf) -> Result<Option<Fan<'static>>, Error> {
     );
     //write(path, "255")?;
     //wait for fan to reach rpm
-    thread::sleep(Duration::from_secs(1));
+    thread::sleep(Duration::from_secs(3));
     //check rpm
     let rpm = read_rpm(path)?;
     if rpm == 0 {
@@ -84,19 +91,43 @@ fn ask_fan(path: &PathBuf) -> Result<Option<Fan<'static>>, Error> {
         return Ok(None);
     }
     println!("Fan reached rpm of {rpm}");
+    println!("Undulating Fan for easier identification.");
+    let undulate = Arc::new(AtomicBool::new(true));
+    let handle = {
+        let copy = path.clone();
+        let undulate = Arc::clone(&undulate);
+        thread::spawn(move || {
+            //ignore failed writes
+            while undulate.load(Ordering::Relaxed) {
+                let _ = write(&copy, "0");
+                thread::sleep(Duration::from_secs(10));
+                let _ = write(&copy, "255");
+                thread::sleep(Duration::from_secs(10));
+            }
+        })
+    };
+
     if Confirm::new()
         .with_prompt("Do you want to add this fan to config?")
         .default(true)
         .interact()
         .unwrap_or(false)
     {
+        undulate.store(false, Ordering::Relaxed);
+        enable_fan_pwm(path, false)?;
+        let _ = handle.join();
+        let name: String = Input::new().with_prompt("Enter fan name").interact_text()?;
+
         Ok(Some(Fan::default()))
     } else {
+        undulate.store(false, Ordering::Relaxed);
+        enable_fan_pwm(path, false)?;
+        let _ = handle.join();
         Ok(None)
     }
 }
 
-pub fn search_fans() -> Vec<Fan<'static>> {
+pub fn search_fans() -> Vec<Fan> {
     let possible_paths = ["/sys/class/hwmon/hwmon*/pwm*[0-9]"];
     search_paths(&possible_paths, ask_fan)
 }
